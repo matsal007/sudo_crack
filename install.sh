e4c7091ccea33201546de6be7a8d5198f1c8bcdd
matsal007 <salvadego2007@gmail.com>
2024-06-06 12:14:10 -0300

    [main] - 'Fixed install script'

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 
+mkdir /home/desenvolvimento/.local/bin
+mkdir /home/desenvolvimento/.local/share/icons
 # Download Inkscape Appimage https://inkscape.org/pt-br/release/inkscape-1.3.2/gnulinux/appimage/dl/ as /home/desenvolvimento/.local/bin/inkscape
-wget https://inkscape.org/pt-br/release/inkscape-1.3.2/gnulinux/appimage/dl/ -O /home/desenvolvimento/.local/bin/inkscape
+wget https://inkscape.org/pt-br/gallery/item/44616/Inkscape-091e20e-x86_64.AppImage -O /home/desenvolvimento/.local/bin/inkscape
 #Download Inkscape logo https://media.inkscape.org/static/images/inkscape-logo.svg as /home/desenvolvimento/.local/share/icons/inkscape.svg
-wget https://media.inkscape.org/static/images/inkscape-logo.svg -O /home/desenvolvimento/.local/share/icons/inkscape.svg
-
+wget https://media.inkscape.org/static/images/inkscape-logo.svg -O /home/desenvolvimento/.local/share/icons/inkscape-logo.svg
 mv ./inkscape.desktop /home/desenvolvimento/.local/share/applications/inkscape.desktop
 chmod +x /home/desenvolvimento/.local/bin/inkscape
 
