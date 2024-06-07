c3ad5ce28a0d34a0c3a5c06c478d14a42ebc79bd
matsal007 <salvadego2007@gmail.com>
2024-06-07 08:46:33 -0300

    [main] - 'Fixed install script'

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 
-mkdir /home/desenvolvimento/.local/bin
-mkdir /home/desenvolvimento/.local/share/icons
-# Download Inkscape Appimage https://inkscape.org/pt-br/release/inkscape-1.3.2/gnulinux/appimage/dl/ as /home/desenvolvimento/.local/bin/inkscape
-wget https://inkscape.org/pt-br/gallery/item/44616/Inkscape-091e20e-x86_64.AppImage -O /home/desenvolvimento/.local/bin/inkscape
-#Download Inkscape logo https://media.inkscape.org/static/images/inkscape-logo.svg as /home/desenvolvimento/.local/share/icons/inkscape.svg
-wget https://media.inkscape.org/static/images/inkscape-logo.svg -O /home/desenvolvimento/.local/share/icons/inkscape-logo.svg
-mv ./inkscape.desktop /home/desenvolvimento/.local/share/applications/inkscape.desktop
-chmod +x /home/desenvolvimento/.local/bin/inkscape
+mkdir $HOME/.local/bin
+mkdir $HOME/.local/share/icons
+wget https://inkscape.org/pt-br/gallery/item/44616/Inkscape-091e20e-x86_64.AppImage -O $HOME/.local/bin/inkscape
+wget https://media.inkscape.org/static/images/inkscape-logo.svg -O $HOME/.local/share/icons/inkscape-logo.svg
+
+touch $HOME/.local/share/applications/inkscape.desktop
+
+echo "[Desktop Entry]" $HOME/.local/share/applications/inkscape.desktop
+echo "Name=Inkscape" $HOME/.local/share/applications/inkscape.desktop
+echo "Type=Application" $HOME/.local/share/applications/inkscape.desktop
+echo "Icon=$HOME/.local/share/icons/inkscape-logo.svg" $HOME/.local/share/applications/inkscape.desktop
+echo "Exec=$HOME/.local/bin/inkscape" $HOME/.local/share/applications/inkscape.desktop
+
+chmod +x $HOME/.local/bin/inkscape
 
